1b1350aafe227db5a7b4bd4accb6e37d1a2de67d
VishalDnyandeoPatil <vp1610858@gmail.com>
2024-01-09 00:07:06 +0530

    kryzen-backend Connected to database

--- kryzen-backend/index.js
+++ kryzen-backend/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.port;
+const {connection}= require('./config/db')
 
 app.get('/', (req, res) => {
   res.send('Hello from Kryzen Backend!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(port, async() => {
+  try {
+    await connection
+    console.log("Kryzen App connected to DB") 
+  } 
+  catch (error) {
+    console.log(error.message);
+  }
 });

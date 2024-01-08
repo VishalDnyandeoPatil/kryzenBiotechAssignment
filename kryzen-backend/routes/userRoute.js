127984554fdc58eb288deb280d683b06fc422212
VishalDnyandeoPatil <vp1610858@gmail.com>
2024-01-09 01:05:17 +0530

    kryzen-backend's usersRoute's registration route done

--- kryzen-backend/routes/userRoute.js
+++ kryzen-backend/routes/userRoute.js
@@ -1,6 +1,32 @@
-const express= require('express');
+const express = require('express');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
-const{users}= require('../model/userModel');
-const{authentication}
+const { users } = require('../model/userModel');
+const { authentication } = require('../middelware/authentication');
+
+const userRoute = express.Router();
+
+userRoute('/registration', async (req, res) => {
+    try {
+        const payload = req.body;
+        const existingUser = await users.findOne({ email: payload.email });
+
+        if (existingUser) {
+            return res.status(409).json({ msg: "User already exists, please login" });
+        }
+        else {
+            const hashPassword = await bcrypt.hashSync(payload.password, 15);
+            payload.password = hashPassword;
+
+            const newUserRegistration = new users(payload);
+            await newUserRegistration.save();
+
+            return res.status(201).json({ msg: "User registration successfully" });
+
+        }
+    }
+    catch (error) {
+        return res.status(500).json({ msg: "Internal server error", error: error.message });
+    }
+});
